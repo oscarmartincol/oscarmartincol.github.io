248beabb31eaeb6606a19660ace3cf5c67870e7e
Oscar Martinez <oscarmartincol@outlook.com>
2025-07-14 16:53:38 -0500

    Eliminar carpeta con problemas.

--- Proyectos/medicalapp/src/main/java/com/oscarmartincol/medicalapp/dto/AppointmentDTO.java
+++
@@ -1,9 +0,0 @@
-package com.oscarmartincol.medicalapp.dto;
-
-import java.time.LocalDateTime;
-
-public class AppointmentDTO {
-    public Long doctorId;
-    public LocalDateTime dateTime;
-    public String reason;
-}
